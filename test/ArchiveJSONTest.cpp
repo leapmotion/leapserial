8840647045313eebbbbad74e303334648c48f685
Walter Gray <wgray@leapmotion.com>
2015-06-24 14:03:18 -0700

    Added the outline of the ArchiveJSONTest

---
+++ test/ArchiveJSONTest.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <gtest/gtest.h>
+
+#include "Serializer.h"
+#include "TestObject.h"
+
+#include "DataStructures/Value.h"
+
+class ArchiveJSONTest :
+  public testing::Test
+{};
+
+using namespace Test;
+TEST_F(ArchiveJSONTest, ReadFromJSON) {
+
+}
