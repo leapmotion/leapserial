9a4fd28d7a8045ea16475f49a305fed0e19524e7
Jason Lokerson <cmercenary@gmail.com>
2015-07-20 06:41:55 -0700

    Add main testing source files

---
+++ src/gtest-all-guard.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include <gtest/gtest-all.cc>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+  auto& listeners = testing::UnitTest::GetInstance()->listeners();
+  testing::InitGoogleTest(&argc, (char**)argv);
+  return RUN_ALL_TESTS();
+}
---
+++ src/gtest-all-guard.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#pragma once
+
+/// <summary>
+/// Delegating main routine, installs an enclosure filter and begins running the tests
+/// </summary>
+int autotesting_main(int argc, const char* argv []);
