6b8b0b3530f90f93be05e10cdb20272b5c8a20d0
Jason Lokerson <cmercenary@gmail.com>
2015-07-20 06:14:55 -0700

    Fix CMake build

---
+++ src/leapserial/test/stdafx.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#ifndef _STDAFX_H
+#define _STDAFX_H
+
+#if _WIN32
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define NOMINMAX
+
+#include <Windows.h>
+
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <utility>
+#include <cassert>
+
+#endif // _WIN32
+
+#endif
---
+++ version.cmake
@@ -0,0 +1,1 @@
+set(leapserial_VERSION 0.1.0)
