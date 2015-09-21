37c8272ab822f551cfe6c343687f748322644980
Jason Lokerson <cmercenary@gmail.com>
2015-09-21 15:07:16 -0700

    Generalize Deserialize and Serialize routines
    We have to do this in order to allow non-std::stream types to be used with all Deserialize and Serialize overrides.

--- LeapSerial.h
+++ LeapSerial.h
@@ -32,37 +32,33 @@ namespace leap {
   /// <summary>
   /// Deserialization routine that returns an std::shared_ptr for memory allocation maintenance
   /// </summary>
-  template<class archive_t, class T, class stream_t>
+  template<class T = void, class archive_t = IArchiveImpl, class stream_t = std::istream>
   std::shared_ptr<T> Deserialize(stream_t&& is) {
     auto retVal = std::make_shared<leap::internal::Allocation<T>>();
-    T* pObj = retVal.get();
+    T* pObj = &retVal->val;
     
     // Initialize the archive with work to be done:
     archive_t ar(is);
     ar.ReadObject(field_serializer_t<T, void>::GetDescriptor(), pObj, retVal.get() );
     
-    return retVal;
+    // Aliasing ctor, because T is a member of Allocation<T> and not a base
+    return { retVal, pObj };
   }
 
   /// <summary>
   /// Deserialization routine that modifies object in place
   /// </summary>
-  template<class archive_t, class T>
-  void Deserialize(std::istream& is, T& obj) {
+  template<class archive_t = IArchiveImpl, class T = void, class stream_t = std::istream>
+  void Deserialize(stream_t&& is, T& obj) {
     // Initialize the archive with work to be done:
     archive_t ar(is);
     ar.ReadObject(field_serializer_t<T, void>::GetDescriptor(), &obj, nullptr);
   }
 
-  template<class archive_t, class T>
-  void Deserialize(std::istream&& is, T& obj) {
-    Deserialize<archive_t, T>(is, obj);
-  }
-
   // Fill a collection with objects serialized to 'is'
   // Returns one past the end of the container
-  template<class T, class archive_t = IArchiveImpl>
-  std::vector<T> DeserializeToVector(std::istream& is) {
+  template<class T, class archive_t = IArchiveImpl, class stream_t = std::istream>
+  std::vector<T> DeserializeToVector(stream_t&& is) {
     std::vector<T> collection;
     while (is.peek() != std::char_traits<char>::eof()) {
       collection.emplace_back();
@@ -70,30 +66,10 @@ namespace leap {
     }
     return collection;
   }
-  
-  template<class T, class archive_t = IArchiveImpl>
-  std::vector<T> DeserializeToVector(std::istream&& is) {
-    return DeserializeToVector<T, archive_t>(is);
-  }
 
   ///I/OArchiveImpl defaulted versions:
   template<class stream_t, class T>
   void Serialize(stream_t&& os, const T& obj) {
     Serialize<OArchiveImpl, stream_t, T>(std::forward<stream_t&&>(os), obj);
-  }
-
-  template<class T, class stream_t>
-  std::shared_ptr<T> Deserialize(stream_t&& is) {
-    return Deserialize<IArchiveImpl, T, stream_t&>(is);
-  }
-
-  template<class T>
-  void Deserialize(std::istream& is, T& obj) {
-    Deserialize<IArchiveImpl, T>(is, obj);
-  }
-
-  template<class T>
-  void Deserialize(std::istream&& is, T& obj) {
-    Deserialize<IArchiveImpl, T>(is, obj);
   }
 }
