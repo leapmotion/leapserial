efaf38a66a931db7f8dc8f9c6044eb138094ceac
Walter Gray <wgray@leapmotion.com>
2015-10-02 13:12:49 -0700

    Added unit test

--- src/leapserial/test/SerializationTest.cpp
+++ src/leapserial/test/SerializationTest.cpp
@@ -1134,12 +1134,14 @@ struct HasChronoMembers {
   std::chrono::seconds one{ 4929 };
   std::chrono::nanoseconds two{ 4929 };
   std::chrono::duration<short, std::ratio<24 * 60 * 60, 1>> three{ 2 };
+  std::chrono::duration<float> four{ 1.2f };
 
   static leap::descriptor GetDescriptor(void) {
     return{
       &HasChronoMembers::one,
       &HasChronoMembers::two,
-      &HasChronoMembers::three
+      &HasChronoMembers::three,
+      &HasChronoMembers::four
     };
   }
 };
@@ -1154,4 +1156,5 @@ TEST_F(SerializationTest, DurationMembers) {
   ASSERT_EQ(hcm.one, re->one) << "Seconds did not deserialize properly";
   ASSERT_EQ(hcm.two, re->two) << "Nanoseconds did not deserialize properly";
   ASSERT_EQ(hcm.three, re->three) << "Custom days duration did not deserialize properly";
+  ASSERT_EQ(hcm.four, re->four) << "Floating point duration did not deserialize properly";
 }
