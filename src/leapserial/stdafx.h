f75a3e7be2dac8e74423808a6600f7407b2d7997
Jason Lokerson <cmercenary@gmail.com>
2015-10-16 11:37:04 -0700

    Add travis support
    Also fix copyright headers

--- src/leapserial/stdafx.h
+++ src/leapserial/stdafx.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 - 2015 Leap Motion. All rights reserved. Proprietary and confidential.
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
 #pragma once
 
 #if _WIN32
