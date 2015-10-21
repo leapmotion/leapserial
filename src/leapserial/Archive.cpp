aacae95c66ffdb91b16d3930b965a7fd1fcff0f8
Jason Lokerson <cmercenary@gmail.com>
2015-10-20 17:51:22 -0700

    Add missing case statements

--- src/leapserial/Archive.cpp
+++ src/leapserial/Archive.cpp
@@ -31,12 +31,20 @@ const char* leap::ToString(serial_atom atom) {
     return "bool";
   case serial_atom::i8:
     return "i8";
+  case serial_atom::ui8:
+    return "ui8";
   case serial_atom::i16:
     return "i16";
+  case serial_atom::ui16:
+    return "ui16";
   case serial_atom::i32:
     return "i32";
+  case serial_atom::ui32:
+    return "ui32";
   case serial_atom::i64:
     return "i64";
+  case serial_atom::ui64:
+    return "ui64";
   case serial_atom::f32:
     return "f32";
   case serial_atom::f64:
