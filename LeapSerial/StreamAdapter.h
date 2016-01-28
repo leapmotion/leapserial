e5a33219fa0892173fbb57361318ecf564b9eca7
Jason Lokerson <cmercenary@gmail.com>
2016-01-27 17:38:48 -0800

    Move input stream adapter to its own header
    Other input stream types are isolated to their own headers, this one should be as well.

---
+++ LeapSerial/StreamAdapter.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "IInputStream.h"
+#include "IOutputStream.h"
+#include <iosfwd>
+#include <memory>
+
+namespace leap {
+  /// <summary>
+  /// Mapping adaptor, allows input streams to be wrapped to support Archive operations
+  /// </summary>
+  class InputStreamAdapter :
+    public IInputStream
+  {
+  public:
+    InputStreamAdapter(std::istream& is) :
+      is(is)
+    {}
+    InputStreamAdapter(std::unique_ptr<std::istream> pis) :
+      is(*pis),
+      pis(std::move(pis))
+    {}
+
+    InputStreamAdapter(const InputStreamAdapter& rhs) :
+      is(rhs.is)
+    {}
+
+  private:
+    std::istream& is;
+
+    // Non-null if we have taken ownership
+    const std::unique_ptr<std::istream> pis;
+
+  public:
+    // IInputStream overrides:
+    bool IsEof(void) const override;
+    std::streamsize Read(void* pBuf, std::streamsize ncb) override;
+    std::streamsize Skip(std::streamsize ncb) override;
+
+    /// <summary>
+    /// Sets the offset on the underlying input stream
+    /// </summary>
+    /// <returns>this</returns>
+    InputStreamAdapter* Seek(std::streamsize off);
+  };
+
+  class OutputStreamAdapter :
+    public IOutputStream
+  {
+  public:
+    OutputStreamAdapter(std::ostream& os) :
+      os(os)
+    {}
+
+    OutputStreamAdapter(std::unique_ptr<std::ostream> pos) :
+      os(*pos),
+      pos(std::move(pos))
+    {}
+
+    OutputStreamAdapter(const OutputStreamAdapter& rhs) :
+      os(rhs.os)
+    {}
+
+    // IOutputStream overrides:
+    bool Write(const void* pBuf, std::streamsize ncb) override;
+
+  private:
+    std::ostream& os;
+
+    // Non-null if we have taken ownership
+    const std::unique_ptr<std::ostream> pos;
+  };
+}
---
+++ src/leapserial/StreamAdapter.cpp
@@ -0,0 +1,31 @@
+#include "stdafx.h"
+#include "StreamAdapter.h"
+
+using namespace leap;
+
+bool InputStreamAdapter::IsEof(void) const {
+  return is.eof();
+}
+
+std::streamsize InputStreamAdapter::Read(void* pBuf, std::streamsize ncb) {
+  is.read((char*)pBuf, ncb);
+  if (is.fail() && !is.eof())
+    return -1;
+  return is.gcount();
+}
+
+std::streamsize InputStreamAdapter::Skip(std::streamsize ncb) {
+  is.ignore(ncb);
+  if (is.fail() && !is.eof())
+    return -1;
+  return is.gcount();
+}
+
+InputStreamAdapter* InputStreamAdapter::Seek(std::streamsize off) {
+  is.seekg(off, std::ios::beg);
+  return this;
+}
+
+bool OutputStreamAdapter::Write(const void* pBuf, std::streamsize ncb) {
+  return (bool)os.write((const char*)pBuf, ncb);
+}
