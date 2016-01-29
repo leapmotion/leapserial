ed288fb318b2432a2d4f404afd2ce33d523e420a
Jason Lokerson <cmercenary@gmail.com>
2016-01-29 14:20:57 -0800

    Fix typoed function call

--- src/leapserialbench/Utility.h
+++ src/leapserialbench/Utility.h
@@ -17,5 +17,5 @@ std::ostream& operator<<(std::ostream& os, const format_duration_t<long long, st
 
 template<typename Rep, typename Period>
 std::ostream& operator<<(std::ostream& os, const format_duration_t<Rep, Period>& rhs) {
-  return os << format_duration{ std::duration_cast<std::chrono::nanoseconds>{rhs.duration} };
+  return os << format_duration_t<long long, std::nano>{ rhs.duration };
 }
