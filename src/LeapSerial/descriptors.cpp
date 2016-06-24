3a9eabb262c4a5a87cffc4d8c2112ae7f073a289
Jason Lokerson <cmercenary@gmail.com>
2016-06-23 19:53:32 -0700

    Fix initialization race in MSVC2013
    MSVC2013 before the CTP doesn't have magic statics, so we need to make sure all of our descriptors are initialized on that platform in advance.

--- src/LeapSerial/descriptors.cpp
+++ src/LeapSerial/descriptors.cpp
@@ -13,6 +13,16 @@ descriptor_entry::descriptor_entry(const std::type_info& ti, const descriptor& (
 
 const descriptor_entry* descriptors::s_pHead = nullptr;
 
+#if _MSC_VER <= 1800
+// Used for MSVC2013 because it doesn't have magic statics, so we have to initialize all
+// of our cached descriptors to ensure that we don't wind up with initialization races.
+static bool global_initializer = [] {
+  for (auto& cur : descriptors{})
+    cur.pfnDesc();
+  return true;
+}();
+#endif
+
 const descriptor_entry* descriptors::Link(descriptor_entry& entry) {
   auto retVal = s_pHead;
   s_pHead = &entry;
